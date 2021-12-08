bb8989ab31ccb136298e70e5477261d744d8dedd
rijento <rijento@gmial.com>
2021-12-08 10:16:03 -0500

    Mostly Done

---
+++ app/src/main/java/net/blindking/weatherrock/controller/InfoController.java
@@ -0,0 +1,41 @@
+package net.blindking.weatherrock.controller;
+
+import android.view.View;
+import android.widget.TextView;
+
+import net.blindking.weatherrock.model.Weather;
+
+public class DialogController {
+    private TextView dialog;
+    private Weather weather;
+    private String infoBookString;
+    private String rockString;
+
+    public DialogController(TextView dialog, Weather weather, String infoBookString, String rockString) {
+        this.dialog = dialog;
+        this.weather = weather;
+        this.infoBookString = infoBookString;
+        this.rockString = rockString;
+
+        this.dialog.setOnClickListener(v -> {
+            this.dialog.animate().setDuration(500).alpha(0.0f);
+            this.dialog.setVisibility(View.GONE);
+        });
+    }
+
+    private void showDialog() {
+        this.dialog.setVisibility(View.VISIBLE);
+        this.dialog.animate().setDuration(500).alpha(1.0f);
+    }
+
+    public void bookClicked() {
+        this.dialog.setText(infoBookString);
+        this.showDialog();
+    }
+
+    public void rockClicked() {
+        String tempString = String.format(rockString, weather.getWindSpeed(), weather.getTemperature());
+        this.dialog.setText(tempString);
+        this.showDialog();
+    }
+}
---
+++ app/src/main/java/net/blindking/weatherrock/controller/MapController.java
@@ -0,0 +1,4 @@
+package net.blindking.weatherrock.controller;
+
+public class MapController {
+}
